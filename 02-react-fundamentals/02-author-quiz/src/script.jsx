f6c02d8285d2913c68cec6bdd0425acba1242e5e
Juan Pablo Cook <juampick@gmail.com>
2016-10-19 17:02:40 -0300

    Added lodash. Finished JSX section

--- 02-react-fundamentals/02-author-quiz/src/script.jsx
+++ 02-react-fundamentals/02-author-quiz/src/script.jsx
@@ -2,21 +2,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import $ from "jquery";
+import _ from "lodash";
 
 
 // Define a component:
 var Quiz = React.createClass({
     propTypes: { //Validation
-        books: React.PropTypes.array.isRequired
+        data: React.PropTypes.array.isRequired
+    },
+    getInitialState: function() {
+        return this.props.data.selectGame();
     },
     render: function() {
         return (
             <div>
-                {
-                    this.props.books.map(function(b, i){
-                        return <Book key={i} title={b} />
-                    })
-                }
+                <div className="row">
+                    <div className="col-md-4">
+                        <img src={this.state.author.imageUrl} className="authorimage col-md-3" />
+                    </div>
+                    <div className="col-md-7">
+                        {this.state.books.map(function(b, i){
+                            return <Book key={i} title={b} />
+                        }, this)}
+                    </div>
+                    <div className="col-md-1"></div>
+                </div>
             </div>
         )
     }
@@ -28,7 +38,7 @@ var Book = React.createClass({
     },
     render: function(){
         return (
-            <div>
+            <div className="answer">
                 <h4>{this.props.title}</h4>
             </div>
         )
@@ -36,11 +46,55 @@ var Book = React.createClass({
 
 });
 
+var data = [
+    {
+        name: 'Mark Twain',
+        imageUrl: 'src/images/authors/marktwain.jpg',
+        books: ['The adventures of Huckleberry Finn']
+    },
+    {
+        name: 'Joseph Conrad',
+        imageUrl: 'src/images/authors/josephconrad.png',
+        books: ['Heart of Darkness']
+    },
+    {
+        name: 'J.K. Rowling',
+        imageUrl: 'src/images/authors/jkrowling.jpg',
+        imageSource: 'Wikimedia Commons',
+        imageAttribution: 'Daniel Ogren',
+        books: ['Harry Potter and the Sorcerers Stone']
+    },
+    {
+        name: 'Stephen King',
+        imageUrl: 'src/images/authors/stephenking.png',
+        imageSource: 'Wikimedia Commons',
+        imageAttribution: 'Pinguino',
+        books: ['The Shining', 'IT']
+    }
+];
+
+data.selectGame = function(){
+    var books = _.shuffle(this.reduce(function(p, c, i){
+        return p.concat(c.books);
+    } ,[])).slice(0, 4);
+
+    var answer = books[_.random(books.length-1)];
+
+    return {
+        books: books,
+        author: _.find(this, function(author){
+            return author.books.some(function(title){
+                return title === answer;
+            });
+        })
+    }
+};
+
 
 // Render a component to the browser:
 ReactDOM.render(
-    <Quiz books={['The Lord of the Rings', 'The super book']}/>,  // What to render (an instance of the Main component)
-    document.getElementById('container') // Where to render it
+    <Quiz data={data}/>,  // What to render (an instance of the Main component)
+    document.getElementById('app') // Where to render it
 );
 
 
