fd99db8c5734497fea9d03bc4762b83bb2caad70
Juan Pablo Cook <juampick@gmail.com>
2016-10-21 19:32:02 -0300

    Added last example for this flux course. Adding browserify so using requires now and 1 file

---
+++ 03-react-flux/05-forum-real-world/src/js/components/ForumHeader.react.js
@@ -0,0 +1,21 @@
+var React = require('react');
+
+var ForumHeader = React.createClass({
+
+    render: function(){
+
+        return (
+            <nav className="navbar-default">
+                <div className="container-fluid">
+                    <div className="navbar-header">
+                        <a className="navbar-brand" href="#">
+                            React Forum
+                        </a>
+                    </div>
+                </div>
+            </nav>
+        );
+    }
+});
+
+module.exports = ForumHeader;
