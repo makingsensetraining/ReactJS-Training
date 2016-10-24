8ed06c1e04a8cc7c82c1c3ae4037ba0d0f7bd778
Juan Pablo Cook <juampick@gmail.com>
2016-10-24 11:20:41 -0300

    Renamed project to browserify. Added some other example (extra of the course) to use webpack config instead of browserify. Working ;)

---
+++ 03-react-flux/06-forum-webpack/src/js/constants/ForumConstants.js
@@ -0,0 +1,7 @@
+
+var ForumConstants = {
+    FORUM_ANSWER_ADDED: 'FORUM_ANSWER_ADDED',
+    FORUM_ANSWER_MARKED_CORRECT: 'FORUM_ANSWER_MARKED_CORRECT'
+};
+
+module.exports = ForumConstants;
---
+++ 03-react-flux/06-forum-webpack/src/js/dispatcher/ForumDispatcher.js
@@ -0,0 +1,5 @@
+
+var Dispatcher = require('flux').Dispatcher;
+var ForumDispatcher = new Dispatcher();
+
+module.exports = ForumDispatcher;
